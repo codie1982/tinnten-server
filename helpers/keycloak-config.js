a5e4aa2035002e16ce38a7f46bb3408cc9e748ae
codie1982 <engin_erol@hotmail.com>
2025-04-25 00:31:51 +0300

    refactor: Replace hardcoded Keycloak configuration with environment variables

--- helpers/keycloak-config.js
+++ helpers/keycloak-config.js
@@ -1,18 +1,14 @@
 // keycloak-config.js
 const Keycloak = require('keycloak-connect');
 
-const KEYCLOAK_BASE_URL = "http://localhost:8080"; // Keycloak URL
-const REALM = "tinnten-realm"; // Keycloak Realm adı
-const CLIENT_ID = "tinnten-client"; // Keycloak Client ID
-const CLIENT_SECRET = "y3P6T54oFpneKZQZdibTmdbKNXSPUwrQ"; // Client Secret (Confidential Clients için)
 
 const keycloak = new Keycloak({ }, {
-  clientId: CLIENT_ID,
+  clientId: process.env.CLIENT_ID,
   bearerOnly: true,
-  serverUrl: KEYCLOAK_BASE_URL,
-  realm: REALM,
+  serverUrl: process.env.KEYCLOAK_BASE_URL,
+  realm: process.env.REALM,
   credentials: {
-    secret: CLIENT_SECRET
+    secret: process.env.CLIENT_SECRET
   }
 });
 
