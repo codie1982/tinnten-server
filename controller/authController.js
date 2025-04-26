63b973502f78bef3d9f807013978b79674c3ddb7
codie1982 <engin_erol@hotmail.com>
2025-04-26 12:23:24 +0300

    refactor: Enable redirect handling in Google authentication flow

--- controller/authController.js
+++ controller/authController.js
@@ -181,10 +181,10 @@ const google = asyncHandler(async (req, res) => {
     const redirectUrl = `${redirecBasetUrl}/google-auth?success=true&token=${token}`;
     console.log("\u2728 Kullan\u0131c\u0131 frontend'e redirect ediliyor:", redirectUrl);
     //res.redirect(`${redirectUrl}/google-auth?success=true&token=${token}`);
-    //res.writeHead(302, { Location: redirectUrl });
+    res.writeHead(302, { Location: redirectUrl });
     console.log("\u2728 Kullan\u0131c\u0131 frontend'e redirect edildi.");
     console.log("\u2728 Google Login Handler Bitti.");
-    //return res.end();
+    return res.end();
   
   } catch (err) {
     console.error("\u274c Genel Google Auth Hatası:", err.message);
